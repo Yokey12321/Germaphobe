7d4790eba0118e5e1268e777280f478c41a2567d
Yokey12321 <92648987+Yokey12321@users.noreply.github.com>
2024-04-24 11:24:29 -0500

    wads

--- Germaphobe/Assets/Scripts/CutsceneScript.cs
+++ Germaphobe/Assets/Scripts/CutsceneScript.cs
@@ -8,15 +8,20 @@ public class CutsceneScript : MonoBehaviour
 
     public Sprite[] slides;
     public Image slideImage;
+    private int index;
 
     void Start()
     {
-        
+        index = 0;
     }
 
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0) == true)
+        {
+            slideImage.sprite = slides[index];
+            index++;
+        }
     }
 
     public void setSlide(int i)
