bdf2f921638003dbba2fd23260203c6f1908b597
Anseroidea <89642979+Anseroidea@users.noreply.github.com>
2024-05-02 11:25:03 -0500

    ddd

---
+++ Germaphobe/Assets/Scripts/Acid.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acid : MonoBehaviour
+{
+
+    private bool mature = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("AllowDeath", 1);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AllowDeath()
+    {
+        mature = true;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (mature && collision.gameObject.name == "stomach_outline")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Germaphobe/Assets/Scripts/AcidSpawner.cs
+++ Germaphobe/Assets/Scripts/AcidSpawner.cs
@@ -22,9 +22,8 @@ public class AcidSpawner : MonoBehaviour
 
     void SpawnAcid()
     {
-        float rad = Random.Range(-1.058f, 1.058f);
-        RaycastHit2D hit = Physics2D.Raycast(wyatt.transform.position, new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)));
+        float rad = Mathf.PI / 2 + Random.Range(-1.058f, 1.058f);
+        RaycastHit2D hit = Physics2D.Raycast(wyatt.transform.position, new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)), Mathf.Infinity,1 <<  8);
         GameObject acid = Instantiate(acidPrefab, hit.point, Quaternion.identity);
-        
     }
 }
